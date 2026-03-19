497cdafae21a9a17cb33f627af7e6b25b7d41a74
李鹏鹏 <lipengpeng-1@thunisoft.com>
2026-03-19 17:13:29 +0800

    Initial commit

---
+++ src/main/java/com/lppnb/ai/myclaw/MyclawApplication.java
@@ -0,0 +1,13 @@
+package com.lppnb.ai.myclaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyclawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lppnb/ai/myclaw/MyclawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lppnb.ai.myclaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
